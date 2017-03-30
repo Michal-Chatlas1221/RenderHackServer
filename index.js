595073d8d8d5cb636d95cbf3cd8b14b68ed087c7
Michał Chatłas <michal.chatlas@espeo.eu>
2017-03-30 21:47:08 +0200

    My hands are typing

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const channel = require('./channel');
 
 channel.init(http);
 
+app.use(express.static('web'));
+app.use(express.static('web-guesser'))
+
 app.get('/', function(req, res) {
   res.send(`
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.js"></script>
@@ -13,6 +16,14 @@ app.get('/', function(req, res) {
         var socket = io.connect('http://178.62.43.178:3000');
     </script>
     `);
+});
+
+app.get('/host', function (req, res) {
+  res.sendFile('web/index.htm')
+});
+
+app.get('/client', function (req, res) {
+  res.sendFile('web-guesser/index.htm')
 });
 
 http.listen(3000, function() {

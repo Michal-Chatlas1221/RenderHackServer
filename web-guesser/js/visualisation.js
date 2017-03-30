2e5e02265dc3f7798ca8be1a007d07440220dcf3
piotrowy <petr.ceranek@gmail.com>
2017-03-30 21:07:43 +0100

    blablabla

--- web-guesser/js/visualisation.js
+++ web-guesser/js/visualisation.js
@@ -1,35 +1,35 @@
 var analyser;
 
 function startVis(audioNode) {
-  analyser = ctx.createAnalyser();
-  analyser.fftSize = 512; // see - there is that 'fft' thing. 
-  audioNode.connect(analyser);
+    analyser = ctx.createAnalyser();
+    analyser.fftSize = 512; // see - there is that 'fft' thing. 
+    audioNode.connect(analyser);
 }
 
 function render(analyser, canvas) {
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
-  analyser.getByteTimeDomainData(dataArray);
-  var canvasCtx = canvas.getContext('2d');
-  canvasCtx.clearRect(0, 0, 2000, 2000);
-  canvasCtx.strokeStyle = '#000';
-  canvasCtx.strokeWidth = '2px';
-  canvasCtx.beginPath();
-  for (var i = 0; i < dataArray.length; i++) {
-    if (i) {
-      canvasCtx.lineTo(i, +dataArray[i]);
-    } else {
-      canvasCtx.moveTo(i, +dataArray[i]);
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+    var canvasCtx = canvas.getContext('2d');
+    canvasCtx.clearRect(0, 0, 2000, 2000);
+    canvasCtx.strokeStyle = '#000';
+    canvasCtx.strokeWidth = '2px';
+    canvasCtx.beginPath();
+    for (var i = 0; i < dataArray.length; i++) {
+        if (i) {
+            canvasCtx.lineTo(i, +dataArray[i]);
+        } else {
+            canvasCtx.moveTo(i, +dataArray[i]);
+        }
     }
-  }
-  canvasCtx.stroke();
-  requestAnimationFrame(function () {
-    render(analyser, canvas);
-  });
+    canvasCtx.stroke();
+    requestAnimationFrame(function() {
+        render(analyser, canvas);
+    });
 }
 
 startVis(window.outNode);
 
-setTimeout(function () {
-  render(analyser, document.getElementById('visualisation'));
+setTimeout(function() {
+    render(analyser, document.getElementById('visualisation'));
 }, 100);
